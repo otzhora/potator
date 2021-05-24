960264e621c6ba2a586b50021cd835ca65bc3a0f
Yuriy Rogachev <yuriy.rogachev@quantori.com>
2021-05-24 17:24:48 +0300

    refactor: refactor argument extraction

--- duplication/__main__.py
+++ duplication/__main__.py
@@ -5,7 +5,20 @@ from duplication.utils import write_formatted_detection_result, make_absolute_pa
 from duplication.profiler import Profile
 
 
-def main(program_args: Namespace) -> None:
+def parse_args() -> Namespace:
+    parser = ArgumentParser(description="Find duplicate code in a directory")
+    parser.add_argument("directory", type=str, help="directory with source code files")
+    parser.add_argument("-d", "--detector", type=str, choices=["Naive", "Filtering"], help="type of detector to use")
+    parser.add_argument("--depth", type=int, default=1, help="depth of adaptive prefix for FilteringDetector")
+    parser.add_argument("-t", "--threshold", type=float, default=0.8, help="threshold of search")
+    parser.add_argument("-g", "--granularity", type=str, default="functions", help="granularity of search")
+    parser.add_argument("-o", "--out", type=str, default="output.html", help="dest of result of the search")
+    args = parser.parse_args()
+    return args
+
+
+def main() -> None:
+    program_args = parse_args()
     directory = make_absolute_path(program_args.directory)
     threshold = program_args.threshold
     granularity = program_args.granularity
@@ -25,12 +38,4 @@ def main(program_args: Namespace) -> None:
 
 
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Find duplicate code in a directory")
-    parser.add_argument("directory", type=str, help="directory with source code files")
-    parser.add_argument("-d", "--detector", type=str, choices=["Naive", "Filtering"], help="type of detector to use")
-    parser.add_argument("--depth", type=int, default=1, help="depth of adaptive prefix for FilteringDetector")
-    parser.add_argument("-t", "--threshold", type=float, default=0.8, help="threshold of search")
-    parser.add_argument("-g", "--granularity", type=str, default="functions", help="granularity of search")
-    parser.add_argument("-o", "--out", type=str, default="output.html", help="dest of result of the search")
-    args = parser.parse_args()
-    main(args)
+    main()
